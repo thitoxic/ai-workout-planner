72563b097ff773ea900618876055e1aafa722c53
Hrishikesh <thitehrishikesh@gmail.com>
2026-06-15 21:28:54 +0530

    feat: add SingleWorkout component for tracking and persisting exercise progress

--- src/features/workouts/singleWorkout/index.tsx
+++ src/features/workouts/singleWorkout/index.tsx
@@ -12,6 +12,18 @@ import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 
 const SingleWorkout = ({ id }: { id: string }) => {
   const [exercises, setExercises] = useState<any>(null);
+  const [userWorkoutData, setUserWorkoutData] = useState<{
+    exerciseId: string;
+    sets: string;
+    reps: string;
+    weight: string;
+  }>({
+    exerciseId: "",
+    sets: "",
+    reps: "",
+    weight: "",
+  });
+  const [openedExercise, setOpenedExercise] = useState<number | null>(0);
 
   useEffect(() => {
     if (id) {
@@ -22,16 +34,62 @@ const SingleWorkout = ({ id }: { id: string }) => {
     }
   }, [id]);
 
+  const postOngoingExerciseData = (exerciseId: number) => {
+    const payload = {
+      exerciseId: exerciseId,
+      sets: userWorkoutData.sets,
+      reps: userWorkoutData.reps,
+      weight: userWorkoutData.weight,
+      completedAt: new Date().toISOString(),
+    };
+    setExercises((prev: any) => {
+      const temp = [...prev];
+      temp[exerciseId].userWorkoutData = payload;
+      return temp;
+    });
+    setOpenedExercise(exerciseId + 1);
+    setUserWorkoutData({
+      exerciseId: "",
+      sets: "",
+      reps: "",
+      weight: "",
+    });
+  };
+
+  const updateWorkoutPlan = () => {
+    const workoutPlans = JSON.parse(
+      localStorage.getItem(`workoutPlan`) || "{}",
+    );
+    workoutPlans.days[id].exercises = exercises;
+    console.log("workoutPlans", workoutPlans);
+    localStorage.setItem(`workoutPlan`, JSON.stringify(workoutPlans));
+  };
+
+  useEffect(() => {
+    updateWorkoutPlan();
+  }, [exercises, id]);
+
+  console.log("exercises", exercises);
+
   const renderSets = (exercise: any) => {
     return (
       <>
-        <ToggleGroup type="single">
+        <ToggleGroup
+          type="single"
+          value={userWorkoutData?.sets?.toString()}
+          onValueChange={(value: string) =>
+            setUserWorkoutData({
+              ...userWorkoutData,
+              sets: value,
+            })
+          }
+        >
           {Array.from({ length: exercise?.sets }, (_, i) => i + 1).map(
             (set) => (
               <ToggleGroupItem
-                className="data-[state=on]:bg-green-300 data-[state=on]:text-white"
+                className="data-[state=off]:border-2 border-gray-300 data-[state=on]:text-white"
                 key={set}
-                value={`set-${set}`}
+                value={set.toString()}
               >
                 {set}
               </ToggleGroupItem>
@@ -55,7 +113,11 @@ const SingleWorkout = ({ id }: { id: string }) => {
                 key={index}
                 className="flex justify-between items-center my-2 w-full mx-auto border-2 rounded-md p-3"
               >
-                <Collapsible className="rounded-md data-[state=open]:bg-white">
+                <Collapsible
+                  className="rounded-md data-[state=open]:bg-white"
+                  open={openedExercise === index}
+                  onOpenChange={(e) => setOpenedExercise(e ? index : null)}
+                >
                   <CollapsibleTrigger asChild>
                     <Button variant="ghost" className="group w-full">
                       {exercise?.name}
@@ -67,12 +129,41 @@ const SingleWorkout = ({ id }: { id: string }) => {
                     <div>Sets: </div>
                     {renderSets(exercise)}
                     <div>Reps: </div>
-                    <ToggleGroup type="single">
-                      <ToggleGroupItem value="reps">
+                    <ToggleGroup
+                      type="single"
+                      value={userWorkoutData?.reps?.toString()}
+                      onValueChange={(value: string) =>
+                        setUserWorkoutData({
+                          ...userWorkoutData,
+                          reps: value,
+                        })
+                      }
+                    >
+                      <ToggleGroupItem value={exercise?.reps?.toString()}>
                         {exercise?.reps}
                       </ToggleGroupItem>
                     </ToggleGroup>
-                    <Button size="xs">
+                    <div>Weights:</div>
+                    <ToggleGroup
+                      type="single"
+                      value={userWorkoutData?.weight?.toString()}
+                      onValueChange={(value: string) =>
+                        setUserWorkoutData({
+                          ...userWorkoutData,
+                          weight: value,
+                        })
+                      }
+                    >
+                      <ToggleGroupItem
+                        value={exercise?.weight?.toString() || "10-20kg"}
+                      >
+                        {exercise?.weight || "10-20kg"}
+                      </ToggleGroupItem>
+                    </ToggleGroup>
+                    <Button
+                      size="xs"
+                      onClick={() => postOngoingExerciseData(index)}
+                    >
                       <Check />
                     </Button>
                   </CollapsibleContent>
